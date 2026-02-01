b2b1226cb18ed53353240dd937b69bf225ed2bff
Abhinay-4052 <33515432+Abhinay-4052@users.noreply.github.com>
2026-02-01 21:06:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Valentine?💝",
+    pageTitle: "Potti baby 🐷 kosam chesina page ",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
